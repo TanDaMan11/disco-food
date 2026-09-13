1aeceb9df71b41a331801963cf13900272f0f5ab
TanDaMan11 <tanner.scarlett@hotmail.com>
2026-09-13 11:26:15 -0700

    Connect meal handbook to Supabase realtime and auth

---
+++ src/lib/supabase.js
@@ -0,0 +1,2 @@
+import { createClient } from '@supabase/supabase-js';
+export const supabase = createClient('https://pgjbbejjsjbviokflqxx.supabase.co','sb_publishable_A7yaX70SzaQTgejC5vPXaw_y9kSRtCK');
